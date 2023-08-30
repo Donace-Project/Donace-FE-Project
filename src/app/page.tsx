35fd5d75b616547efe576f2f73930171b2e83a64
trandat1114 <cctalk1114@gmail.com>
2023-08-30 21:11:24 +0700

    fuck ups

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from '@/components/theme/toggle-theme-button'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      {/* <Button>Hello</Button> */}
+      <ModeToggle />
+    </div>
+  )
+}
