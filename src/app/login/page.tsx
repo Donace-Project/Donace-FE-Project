a7c88a4002f5bb54c0ed1cfcc547c76df25cff1e
GiaBao <nguyenhiengiabao12@gmail.com>
2023-09-06 16:20:46 +0700

    Adding Navbar to Index

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 import { NavbarLogin } from "@/components/layout/navbar-login";
 import { LoginFormCard } from "@/components/login-page/login-card";
+import { LoginVerify } from "@/components/login-verify/login-verify-code";
 
 export default function Page() {
   return (
@@ -12,5 +13,10 @@ export default function Page() {
           <LoginFormCard/>
         </div>
       </div>
+      {/* <div className="px-4 w-full bg-white">
+        <div className="h-screen flex justify-center items-center p-0">
+            <LoginVerify/>
+          </div>
+      </div> */}
       </div>)
     }
