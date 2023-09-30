6bd81314c8c5686dbdc4bd93672eeb972e530f4d
trandat1114 <cctalk1114@gmail.com>
2023-09-30 16:21:11 +0700

    ok

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link"
 import { PropsWithChildren } from "react"
-// import { LoginNRegister } from "@/components/auth/loginNRegister"
-import { ModeToggle } from "@/components/theme/toggle-theme-button"
 import { Input } from "@/components/ui/input"
 import Header from "@/components/layout/header"
 
@@ -10,7 +8,7 @@ import Header from "@/components/layout/header"
 export const Layout = ({ children }: PropsWithChildren) => {
     return (
         <div className="relative flex min-h-screen flex-col">
-            <div className="supports-backdrop-blur:bg-background/60 sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur">
+            {/* <div className="supports-backdrop-blur:bg-background/60 sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur">
                 <div className="container flex h-14 items-center">
                     <Header />
 
@@ -19,13 +17,13 @@ export const Layout = ({ children }: PropsWithChildren) => {
                             <Input className="inline-flex items-center rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2 relative w-full justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-64" type="text" placeholder="Search..." />
                         </div>
                         <nav className="flex items-center">
-                            {/* <LoginNRegister /> */}
+                            <LoginNRegister />
                             <ModeToggle />
                         </nav>
                     </div>
 
                 </div>
-            </div>
+            </div> */}
             {children}
         </div>
     )
